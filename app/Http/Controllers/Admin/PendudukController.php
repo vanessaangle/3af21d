871805f313172957f3326f6903680efa8df122fc
Pande Putu Widya Oktapratama <com.widyaop@gmail.com>
2019-05-02 14:13:31 +0800

    Done penduduk

--- app/Http/Controllers/Admin/PendudukController.php
+++ app/Http/Controllers/Admin/PendudukController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Penduduk;
+use Carbon\Carbon;
+use Alert;
 
 class PendudukController extends Controller
 {
@@ -20,12 +23,24 @@ class PendudukController extends Controller
             ['value' => 'Laki Laki','name' => 'Laki Laki'],
             ['value' => 'Perempuan','name' => 'Perempuan']
         ];
-
+        $agama = [
+            ['value' => 'Hindu','name' => 'Hindu'],
+            ['value' => 'Islam','name' => 'Islam'],
+            ['value' => 'Katolik','name' => 'Katolik'],
+            ['value' => 'Protestan','name' => 'Protestan'],
+            ['value' => 'Budha','name' => 'Budha'],
+            ['value' => 'Konghuchu','name' => 'Konghuchu'],
+        ];
         return [
            ['label' => 'NIK','name' => 'nik','view_index' => true],
-           ['label' => 'Nama','name' => 'name','view_index' => true],
+           ['label' => 'Nama','name' => 'nama','view_index' => true],
            ['label' => 'Jenis Kelamin', 'name' => 'jenis_kelamin','type' => 'select', 'option' => $jenis, 'view_index' => true],
-           ['label' => '']
+           ['label' => 'Tempat Lahir','name' => 'tempat_lahir','view_index' => true],
+           ['label' => 'Tanggal Lahir','name' => 'tgl_lahir', 'type' => 'datepicker'],
+           ['label' => 'Agama','name' => 'agama','type' => 'select','option' => $agama,'view_index' => true],
+           ['label' => 'Pekerjaan','name' => 'pekerjaan','view_index' => true],
+           ['label' => 'Golongan Darah','name' => 'golongan_darah','view_index' => true],
+           ['label' => 'Status','name' => 'status','view_index' => true]
         ];
     }
     /**
@@ -35,7 +50,11 @@ class PendudukController extends Controller
      */
     public function index()
     {
-        //
+        $data = Penduduk::where('desa_id',auth()->user()->desa_id)
+            ->get();
+        $form = $this->form();
+        $template = (object) $this->template;
+        return view('admin.petugas.index',compact('data','form','template'));
     }
 
     /**
@@ -45,7 +64,9 @@ class PendudukController extends Controller
      */
     public function create()
     {
-        //
+        $form = $this->form();
+        $template = (object) $this->template;
+        return view('admin.desa.create',compact('form','template'));
     }
 
     /**
@@ -56,7 +77,32 @@ class PendudukController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nik' => 'required|unique:penduduk,nik',
+            'nama' => 'required',
+            'jenis_kelamin' => 'required',
+            'tempat_lahir' => 'required',
+            'tgl_lahir' => 'required',
+            'agama' => 'required',
+            'pekerjaan' => 'required',
+            'golongan_darah' => 'required',
+            'status' => 'required'
+        ]);
+
+        Penduduk::create([
+            'desa_id' => auth()->user()->desa_id,
+            'nik' => $request->nik,
+            'nama' => $request->nama,
+            'jenis_kelamin' => $request->jenis_kelamin,
+            'tempat_lahir' => $request->tempat_lahir,
+            'tgl_lahir' => Carbon::parse($request->tgl_lahir)->format('Y-m-d'),
+            'agama' => $request->agama,
+            'pekerjaan' => $request->pekerjaan,
+            'golongan_darah' => $request->golongan_darah,
+            'status' => $request->status
+        ]);
+        Alert::make('success','Berhasil simpan data');
+        return redirect(route($this->template['route'].'.index'));
     }
 
     /**
@@ -67,7 +113,10 @@ class PendudukController extends Controller
      */
     public function show($id)
     {
-        //
+        $template = (object) $this->template;
+        $form = $this->form();
+        $data = Penduduk::findOrFail($id);
+        return view('admin.desa.show',compact('template','form','data'));
     }
 
     /**
@@ -78,7 +127,10 @@ class PendudukController extends Controller
      */
     public function edit($id)
     {
-        //
+        $template = (object) $this->template;
+        $form = $this->form();
+        $data = Penduduk::findOrFail($id);
+        return view('admin.desa.edit',compact('template','form','data'));
     }
 
     /**
@@ -90,7 +142,30 @@ class PendudukController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'nik' => 'required|unique:penduduk,nik,'.$id,
+            'nama' => 'required',
+            'jenis_kelamin' => 'required',
+            'tempat_lahir' => 'required',
+            'tgl_lahir' => 'required',
+            'agama' => 'required',
+            'pekerjaan' => 'required',
+            'golongan_darah' => 'required',
+            'status' => 'required'
+        ]);
+        Penduduk::find($id)->update([
+            'nik' => $request->nik,
+            'nama' => $request->nama,
+            'jenis_kelamin' => $request->jenis_kelamin,
+            'tempat_lahir' => $request->tempat_lahir,
+            'tgl_lahir' => Carbon::parse($request->tgl_lahir)->format('Y-m-d'),
+            'agama' => $request->agama,
+            'pekerjaan' => $request->pekerjaan,
+            'golongan_darah' => $request->golongan_darah,
+            'status' => $request->status
+        ]);
+        Alert::make('success','Berhasil simpan data');
+        return redirect(route($this->template['route'].'.index'));
     }
 
     /**
@@ -101,6 +176,8 @@ class PendudukController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Penduduk::find($id)->delete();
+        Alert::make('success','Berhasil hapus data');
+        return redirect(route($this->template['route'].'.index'));
     }
 }
