26eab5764aa7872feff859126a026f790cadc085
Yoga <igedeyoga97@gmail.com>
2019-07-03 17:41:10 +0800

    update oka

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -65,19 +65,30 @@
                         <!-- User Account: style can be found in dropdown.less -->
                         <li class="dropdown user user-menu">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <img src="{{Auth::guard('admin')->user()->foto != '' ? asset('image').'/'.Auth::guard('admin')->user()->foto : asset('admin-lte').'/dist/img/user2-160x160.jpg'}}" class="user-image" alt="User Image">
+                                <img src="{{Auth::guard('admin')->user()->foto != '' ? asset('image').'/'.Auth::guard('admin')->user()->foto : asset('admin-lte').'/dist/img/avatar5.png'}}" class="user-image" alt="User Image">
                                 <span class="hidden-xs">{{Auth::guard('admin')->user()->nama}}</span>
                             </a>
                             <ul class="dropdown-menu">
                                 <!-- User image -->
+                                @if (Auth::guard('admin')->user()->role == 'Admin' )
                                 <li class="user-header">
-                                    <img src="{{Auth::guard('admin')->user()->foto != '' ? asset('image').'/'.Auth::guard('admin')->user()->foto : asset('admin-lte').'/dist/img/user2-160x160.jpg'}}" class="img-circle" alt="User Image">
+                                    <img src="{{Auth::guard('admin')->user()->foto != '' ? asset('image').'/'.Auth::guard('admin')->user()->foto : asset('admin-lte').'/dist/img/avatar5.png'}}" class="img-circle" alt="User Image">
+
+                                    <p>
+                                        {{Auth::guard('admin')->user()->role}}                                       
+                                    </p>
+                                </li>
+
+                                @else
+                                    <li class="user-header">
+                                    <img src="{{Auth::guard('admin')->user()->foto != '' ? asset('image').'/'.Auth::guard('admin')->user()->foto : asset('admin-lte').'/dist/img/avatar5.png'}}" class="img-circle" alt="User Image">
 
                                     <p>
                                         {{Auth::guard('admin')->user()->role}}                                       
                                     </p>
                                     <small>{{Auth::guard('admin')->user()->desa->nama_desa}}</small>
                                 </li>
+                                @endif
 
                                 <!-- Menu Footer-->
                                 <li class="user-footer">
@@ -102,15 +113,15 @@
             <!-- sidebar: style can be found in sidebar.less -->
             <section class="sidebar">
                 <!-- Sidebar user panel -->
-                <div class="user-panel">
+                <!-- <div class="user-panel">
                     <div class="pull-left image">
-                        <img src="{{Auth::guard('admin')->user()->foto != '' ? asset('image').'/'.Auth::guard('admin')->user()->foto : asset('admin-lte').'/dist/img/user2-160x160.jpg'}}" class="img-circle" alt="User Image">
+                        <img src="{{Auth::guard('admin')->user()->foto != '' ? asset('image').'/'.Auth::guard('admin')->user()->foto : asset('admin-lte').'/dist/img/avatar5.png'}}" class="img-circle" alt="User Image">
                     </div>
                     <div class="pull-left info">
                         <p>{{Auth::guard('admin')->user()->nama}}</p>
                         <a href="#"><i class="fa fa-circle text-success"></i> {{Auth::guard('admin')->user()->role}}</a>
                     </div>
-                </div>  
+                </div>   -->
                 <!-- sidebar menu: : style can be found in sidebar.less -->
                 <ul class="sidebar-menu" data-widget="tree">
                     <li class="header">MAIN NAVIGATION</li>
@@ -193,10 +204,9 @@
         
         <footer class="main-footer">
             <div class="pull-right hidden-xs">
-                <b>Version</b> 2.4.0
+
             </div>
-            <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
-            reserved.
+            <strong>Copyright &copy; 2019 <a target="__blank" href="https://badungkab.go.id">Pemerintah Kabupaten Badung </a>
         </footer>
         
         <!-- /.control-sidebar -->
